70c45ebfd6051ff1e428d4828ada06b9f549927d
IgnaCat <itsigna02@gmail.com>
2019-11-14 08:51:20 -0300

    Solucion de que habia borrado unas cosas de willy xd

--- Repairs/RepairApp/admin.py
+++ Repairs/RepairApp/admin.py
@@ -24,12 +24,40 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Count
 from django.db.models.functions import TruncDay
 import json
+from django.contrib.auth.forms import *
+from django.contrib.auth.models import Permission
 
 csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
 
 admin.site.site_header = _("Repairs Administration")
 admin.site.site_title = _("My Repairs Admin")
+class MyUserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+
+    class Meta:
+        model = MyUser
+        fields = ()
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(MyUserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
 class ProductoInline(admin.TabularInline):
